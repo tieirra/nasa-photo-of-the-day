506ad2e310fe586fdeb0f901853db32eca5ada2c
user <user@users-MacBook-Air.local>
2020-06-11 09:18:33 -0500

    adding the useeffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
-  const [details , setDetails] = useState()
+  const [details , setDetails] = useState({})
+
+  useEffect(() => {
+   axios.get ('http://api.nasa.gov/planetary/apod?api_key=VmyAhAymcPU8zBtQus3iH5vQbGQ5jUkCiG5qpdVz')
+   
+   .then ((response) =>{
+    console.log(response.data)
+    setDetails(response.data)
+   })
+
+   .catch( error => {
+    console.log(error);
+})
+   
+  }, []);
+
+  console.log(details)
+  
   return (
     <div className="App">
       <p>
